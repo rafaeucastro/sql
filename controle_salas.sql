829021d26d02eaab56d634599757fb05e094097e
Rafaeu Castro <castrorafael456@gmail.com>
2023-03-17 11:23:58 -0300

    Correção de bugs controle_salas

--- controle_salas.sql
+++ controle_salas.sql
@@ -12,7 +12,7 @@ show columns from professores;
 create TABLE salas(
     id varchar(4) PRIMARY KEY,
     descricao varchar(50),
-    esta_livre boolean not null default 1,
+    esta_livre boolean not null default true,
     professor_responsavel int,
     FOREIGN key(professor_responsavel) REFERENCES professores(id)
 );
@@ -49,11 +49,15 @@ INSERT INTO professores(nome) values ("Alex Lacerda");
 INSERT INTO professores(nome) values ("Diana Mabel");
 INSERT INTO professores(nome) values ("Manoel Lopes");
 
+select  * from professores;
+
 INSERT INTO salas(id, descricao, esta_livre) values ("B1S2", "Sala de aula", TRUE);
 INSERT INTO salas(id, descricao, esta_livre, professor_responsavel) values ("B1S3", "Laboratório de Redes", FALSE, 1);
 INSERT INTO salas(id, descricao, esta_livre, professor_responsavel) values ("B1S1", "Laboratório de informática", FALSE, 3);
 
-CREATE VIEW vw_salas_livres AS
+select * from salas;
+
+CREATE VIEW vw_salas_ocupadas AS
 SELECT s.id AS "Código",
 s.descricao AS "Descrição",
 s.esta_livre AS "Está livre",
@@ -62,9 +66,17 @@ FROM salas AS s
 inner JOIN professores AS p
 ON s.professor_responsavel = p.id;
 
-select * from vw_salas_livres;
+CREATE VIEW vw_salas_livres AS
+SELECT s.id AS "Código",
+s.descricao AS "Descrição",
+s.esta_livre AS "Está livre"
+FROM salas AS s
+where esta_livre = true;
 
-ALTER VIEW vw_salas_livres AS
+select * from vw_salas_livres;
+select * from vw_salas_ocupadas;
+
+ALTER VIEW vw_salas_ocupadas AS
 SELECT s.id AS "Código",
 s.descricao AS "Descrição",
 s.disponivel AS "Está disponível",
@@ -73,9 +85,9 @@ FROM salas AS s
 inner JOIN professores AS p
 ON s.professor_responsavel = p.id;
 
-select * from vw_salas_livres;
+select * from vw_salas_ocupadas;
 
 -- Atualizando status da sala
 update salas set esta_livre = true where professor_responsavel = 1; 
 
-select * from vw_salas_livres;
+select * from vw_salas_ocupadas;
